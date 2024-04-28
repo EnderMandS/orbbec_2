4fbf73b1767eb00fed26bb544370ed89d0359bf1
EnderMandS <952025764@qq.com>
2024-04-28 13:43:50 +0800

    add publish /odom

--- basic_tf/scripts/tf_listen.py
+++ basic_tf/scripts/tf_listen.py
@@ -1,23 +1,43 @@
 #!/usr/bin/env python
+
+# Listen the transform from odom to base_link
+# Publish it to topic nav_msgs::Odomtery /odom
+
 import rospy
-import tf
+import tf2_ros
+import tf_conversions
+from nav_msgs.msg import Odometry
 
-def listener():
-    rospy.init_node('tf_listener')
+def tf_to_odom(tf):
+    odom = Odometry()
+    odom.header = tf.header
+    odom.child_frame_id = 'base_link'
+    odom.pose.pose.position.x = tf.transform.translation.x
+    odom.pose.pose.position.y = tf.transform.translation.y
+    odom.pose.pose.position.z = tf.transform.translation.z
+    odom.pose.pose.orientation = tf.transform.rotation
+    return odom
 
-    tf_listener = tf.TransformListener()
+def main():
+    rospy.init_node('tf_to_odom')
 
-    rate = rospy.Rate(1.0)
+    tf_buffer = tf2_ros.Buffer(rospy.Duration(1.0)) # tf buffer length
+    tf_listener = tf2_ros.TransformListener(tf_buffer)
+
+    odom_pub = rospy.Publisher('/odom', Odometry, queue_size=1)
+
+    rospy.loginfo('Listening from odom to base_link, publish it to /odom')
+
+    rate = rospy.Rate(100.0)
     while not rospy.is_shutdown():
         try:
-            (trans,rot) = tf_listener.lookupTransform('camera_intel_infra2_optical_frame', \
-                                                    'camera_intel_imu_optical_frame', rospy.Time(0))
-            rospy.loginfo("T: %s", str(trans))
-            rospy.loginfo("R: %s", str(rot))
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            trans = tf_buffer.lookup_transform('odom', 'base_link', rospy.Time.now(), rospy.Duration(1.0))
+            odom_pub.publish(tf_to_odom(trans))
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+            # rospy.logwarn("%s", e)
             continue
-
-        rate.sleep()
+        finally:
+            rate.sleep()
 
 if __name__ == '__main__':
-    listener()
+    main()
